8efea019172b3340083b4a830a0b1ad5417a37d0
Blur11602 <50123021+Blur11602@users.noreply.github.com>
2022-12-12 01:19:53 +1100

    Add files via upload

---
+++ dividend_reinvestment_calculator.py
@@ -0,0 +1,58 @@
+# Dividend re-investment calculator
+# Tested against https://www.tipranks.com/tools/dividend-calculator
+import datetime
+
+print("Welcome to the dividend re-investment calculator!")
+print("This calculator will only work for annual dividend distributions.")
+
+attempt = 1
+loop = True
+while loop == True:
+	try:
+		print(f"\nAttempt {attempt}")
+		price = float(input("Enter the current price of the stock ($):\n > "))
+		dividend = float(input("Enter the current dividend yield (%):\n > "))
+		shares = float(input("Enter the number of shares you own:\n > "))
+		loop = False
+	except Exception as e:
+		attempt += 1
+		loop = False if e == KeyboardInterrupt else print("Please enter a valid decimal or integer.")
+
+
+if dividend > 0.5:
+	dividend /= 100
+
+total = price * shares * dividend
+initial_shares = shares
+initial_dividend = dividend
+initial_price = price
+initial_total = shares * price
+
+print(f"\nIf the price does not change at all you will receive roughly ${total:.2f} in dividends per year without reinvestment.")
+
+cont = input("\nWould you like to reinvest your dividends each year? (y/n)\n > ")
+if cont.lower() == "y":
+	years = int(input("How many years of reinvesting would you like to calculate?\n > "))
+	print(f"\nIf the price does not change at all...")
+	print(f"""{datetime.datetime.now().year}:
+	{shares:.2f} shares
+	Worth ${(shares * price):.2f}
+	${total:.2f} p.a.""")
+	for year in range(1, years + 1):
+		shares += total / price
+		total = shares * price * dividend
+		print(f"""{datetime.datetime.now().year + year}:
+	{shares:.2f} shares
+	Worth ${(shares * price):.2f}
+	${total:.2f} p.a.
+
+	{initial_shares:.2f} initial shares
+	${initial_total:.2f} initial total
+	${(shares * price) - initial_total:.2f} total profit""")
+elif cont.lower() == "n":
+	years = int(input("How many years would you like to calculate?\n > "))
+	print(f"\nIf the price does not change at all...")
+	total = shares * price * dividend * years
+	print(f"""	{shares:.2f} shares
+	Worth ${(shares * price):.2f}
+	${total:.2f} total dividends""")
