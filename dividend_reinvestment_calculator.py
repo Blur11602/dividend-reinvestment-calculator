680d842d0162ca9afd80ec9e82af266f7b4d6ebe
Blur11602 <50123021+Blur11602@users.noreply.github.com>
2022-12-12 14:35:39 +1100

    added asx api for price and dividend yield data
    will not work for stocks not on the ASX

--- dividend_reinvestment_calculator.py
+++ dividend_reinvestment_calculator.py
@@ -1,24 +1,44 @@
 # Dividend re-investment calculator
 # Tested against https://www.tipranks.com/tools/dividend-calculator
 import datetime
+import requests
+import json
 
 print("Welcome to the dividend re-investment calculator!")
 print("This calculator will only work for annual dividend distributions.")
+
+# function to get stock price from the ASX API
+def get_stock_info(symbol):
+	try:
+		# make a GET request to the ASX API
+		response = requests.get(f"https://www.asx.com.au/asx/1/share/{symbol}")
+
+		# parse the response as JSON
+		json_data = json.loads(response.text)
+
+		data = [json_data["last_price"], json_data["annual_dividend_yield"]]
+
+		# return the stock price
+		return data
+	except:
+		# return 0 if an error occurred
+		return 0
 
 attempt = 1
 loop = True
 while loop == True:
 	try:
 		print(f"\nAttempt {attempt}")
-		price = float(input("Enter the current price of the stock (in $):\n > "))
-		dividend = float(input("Enter the current dividend yield (as a %):\n > "))
+		symbol = input("Enter the ASX stock symbol (only Australian stocks or securities are accepted):\n > ")
+		price = float(get_stock_info(symbol)[0])
+		dividend = float(get_stock_info(symbol)[1])
+		dividend /= 100
 		shares = float(input("Enter the number of shares you own:\n > "))
 		loop = False
 	except Exception as e:
 		attempt += 1
-		loop = False if e == KeyboardInterrupt else print("Please enter a valid decimal or integer. Do not include symbols")
+		loop = False if e == KeyboardInterrupt else print("Please enter a valid decimal or integer.")
 
-dividend /= 100
 
 total = price * shares * dividend
 initial_shares = shares
@@ -26,7 +46,7 @@ initial_dividend = dividend
 initial_price = price
 initial_total = shares * price
 
-print(f"\nIf the price does not change at all you will receive roughly ${total:.2f} in dividends per year without reinvestment.")
+print(f"\nIf the price does not change at all you will receive roughly ${total:.2f} in dividends per year without reinvestment.\n[{get_stock_info(symbol)[1]}% of ${price:.2f} per share]")
 
 cont = input("\nWould you like to reinvest your dividends each year? (y/n)\n > ")
 if cont.lower() == "y":
@@ -35,14 +55,14 @@ if cont.lower() == "y":
 	print(f"""{datetime.datetime.now().year}:
 	{shares:.2f} shares
 	Worth ${(shares * price):.2f}
-	${total:.2f} p.a.""")
+	${total:.2f} in dividends p.a.""")
 	for year in range(1, years + 1):
 		shares += total / price
 		total = shares * price * dividend
 		print(f"""{datetime.datetime.now().year + year}:
 	{shares:.2f} shares
 	Worth ${(shares * price):.2f}
-	${total:.2f} p.a.
+	${total:.2f} in dividends p.a.
 
 	{initial_shares:.2f} initial shares
 	${initial_total:.2f} initial total
